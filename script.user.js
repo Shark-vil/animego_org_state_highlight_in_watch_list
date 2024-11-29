c88426843a75ccf45c59e23d749fada5331a1367
Shark-vil <sharkvil228@gmail.com>
2024-11-29 22:05:47 +0300

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AnimeGo Scraper - Color Indication of Viewed
 // @namespace    https://github.com/Shark-vil/animego_scraper_color_indication
-// @version      1.2.2
+// @version      1.2.3
 // @description  Скрипт для сайта AnimeGo.org, который помечает или скрывает в общем списке уже просмотренные аниме.
 // @author       Shark_vil
 // @icon         https://raw.githubusercontent.com/Shark-vil/animego_scraper_color_indication_of_viewed/refs/heads/master/icon.png
@@ -180,6 +180,8 @@
         
         return new Promise((resolve, reject) => {
             $iframe.on("load", async () => {
+                await delay(1500);
+
                 const iframeDocument = $iframe[0].contentDocument;
                 const $tableBody = $(iframeDocument).find('tbody[data-loaded="true"]');
         
