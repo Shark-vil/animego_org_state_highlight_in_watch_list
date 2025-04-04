99fdc086c2d43c3a585772692236b57c8a81c04f
Shark-vil <sharkvil228@gmail.com>
2025-04-04 14:05:55 +0300

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Animego.org - Подсветка аниме в списке цветом
 // @namespace    https://github.com/Shark-vil/animego_org_state_highlight_in_watch_list
-// @version      1.3.8
+// @version      1.3.9
 // @description  Скрипт помечает аниме цветом состояния просмотра, а также может скрывать уже просмотренные или брошенные аниме.
 // @author       Shark_vil
 // @icon         https://raw.githubusercontent.com/Shark-vil/animego_org_state_highlight_in_watch_list/refs/heads/master/icon.png
@@ -10,6 +10,7 @@
 // @match        https://animego.club/*
 // @match        https://animego.one/*
 // @match        https://animego.bz/*
+// @match        https://animego.ac/*
 // @grant        none
 // @updateURL    https://github.com/Shark-vil/animego_org_state_highlight_in_watch_list/raw/refs/heads/master/script.user.js
 // @downloadURL  https://github.com/Shark-vil/animego_org_state_highlight_in_watch_list/raw/refs/heads/master/script.user.js
